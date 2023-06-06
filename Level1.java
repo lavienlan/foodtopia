82e22d5007fadd27155cf06c1a1edcc442b50862
lavienlan <130789593+lavienlan@users.noreply.github.com>
2023-06-06 19:40:01 -0400

    level 1 complete + mouseclick sound added

--- Level1.java
+++ Level1.java
@@ -6,9 +6,13 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.Image;
 import java.io.*;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class Level1 implements MouseListener {
-    Font diloWorldL, diloWorldS, diloWorldSS, pixeltype, dogicaB, dogicaBM;
+    Font diloWorldL, diloWorldS, diloWorldSS, dogicaB, dogicaBM, dogica_text;
     Color sky = new Color(169, 208, 245);
     Color project = new Color(253, 235, 195, 150);
     JFrame frame;
@@ -35,8 +39,17 @@ public class Level1 implements MouseListener {
         frame.setVisible(true);
     }
 
+
     @Override
     public void mouseClicked (MouseEvent e) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("click.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception ex) {
+        }
+
         int x = e.getX();
         int y = e.getY();
         if (instructionPoint < 5 ) {
@@ -73,10 +86,11 @@ public class Level1 implements MouseListener {
             if (!character_select.equals("")) {
                 if (x >= 730 && y >= 20 && y <= 55 && x <= 760) { //&& y <= 30 && x <= 790) {
                     character_select = "";
+                    info_slider = 0;
                     System.out.println(character_select);
                     frame.repaint();
                 } else if (x >= 680 && y >= 300 && y <= 350 && x <= 710) {
-                    if (info_slider < 3) {
+                    if (info_slider < 2) {
                         info_slider++;
                     }
                 } else if (x >= 630 && y >= 300 && y <= 350 && x <= 660) {
@@ -126,8 +140,8 @@ public class Level1 implements MouseListener {
                 diloWorldL = Font.createFont(Font.TRUETYPE_FONT, new File("DiloWorld.ttf")).deriveFont(75f);
                 diloWorldS = Font.createFont(Font.TRUETYPE_FONT, new File("DiloWorld.ttf")).deriveFont(40f);
                 diloWorldSS = Font.createFont(Font.TRUETYPE_FONT, new File("DiloWorld.ttf")).deriveFont(50f);
-                pixeltype = Font.createFont(Font.TRUETYPE_FONT, new File("Pixeltype.ttf")).deriveFont(75f);
                 dogicaBM = Font.createFont(Font.TRUETYPE_FONT, new File("dogicapixelbold.ttf")).deriveFont(11f);
+                dogica_text = Font.createFont(Font.TRUETYPE_FONT, new File("dogicapixelbold.ttf")).deriveFont(12f);
                 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
                 ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File ("DiloWorld.ttf")));
                 ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File ("Pixeltype.ttf")));
@@ -156,11 +170,9 @@ public class Level1 implements MouseListener {
                 g.fillOval(45, 45, 40,40);
 
                 // character imports
-                // .getScaledInstance(200, 200, Image.SCALE_DEFAULT)
-                //Image donna = t.getImage("characters/donna.png");
+    
                 Toolkit t = Toolkit.getDefaultToolkit();
-            
-
+        
                 Image cantieScaled = cantie.getScaledInstance(120, 120, Image.SCALE_DEFAULT);
                 g.drawImage(cantieScaled, -10, 345, this);
 
@@ -233,47 +245,60 @@ public class Level1 implements MouseListener {
 
                 g.setFont(diloWorldSS);
                 g.setColor(Color.WHITE);
-
                 
                 switch (character_select) {
                     case "wally":
                         g.drawString("Wally the Water", 50, 80);
                         Image wallyScaled = wally.getScaledInstance(350, 350, Image.SCALE_DEFAULT);
                         g.drawImage(wallyScaled, 0, 100, this);
+                        character_info = "Say hello to Wally the Water! Wally is not like the other characters in Foodtopia because he is not a food. Instead, Wally is a friendly water droplet. He's clear and cool, just like fresh water you drink!";
+                        personality = "Wally is super important for our bodies. He helps us stay healthy and strong. When we drink water, Wally goes inside us and does amazing things. He keeps us hydrated, helps our muscles work, and keeps our skin looking good!";
+                        advice = "Remember to drink water every day to keep Wally happy and your body healthy. Wally loves to be your best friend, so make sure to take sips of water throughout the day.";
                         break;
 
                     case "rhonda":
                         g.drawString("Rhonda the Red Velvet Cake", 50, 80);
                         Image rhondaScaled = rhonda.getScaledInstance(350, 350, Image.SCALE_DEFAULT);
                         g.drawImage(rhondaScaled, 0, 100, this);
+                        character_info = "Uh-oh, here comes Rhonda the Red Velvet Cake! She might look delicious, but she's not as friendly as she seems. Rhonda is made from ingredients that can be bad for your health if you have too much of her.";
+                        personality = "Rhonda may taste yummy, but eating too much of her and other unhealthy treats can be risky. They can make you gain too much weight, get sick, or even hurt your teeth. It's important to be careful and not let Rhonda trick you!";
+                        advice = "Remember, Rhonda is a special treat for special times. It's okay to enjoy her every once in a while, but you should focus on eating healthy foods most of the time. Look for snacks made with better things or try other tasty options that are good for you.";
                         break;
 
                     case "donna":
                         g.drawString("Donna the Donut", 50, 80);
                         Image donnaScaled = donna.getScaledInstance(350, 350, Image.SCALE_DEFAULT);
                         g.drawImage(donnaScaled, 0, 100, this);
-                        character_info = "Meet Darry the Donut! He looks yummy and sweet on the outside, covered in sugar. However, there's more to Darry than meets the eye. Once you take a bite, you'll discover his true nature—a sinister and chewy piece of dough lurking within!";
-                        personality = "Darry is a tasty treat, but you need to be careful. Eating too much of him can make you sick. He has lots of sugar, which is not good for your body. It can give you bad diseases and make your energy go up and then crash down.";
-                        ingredients = "Ingredients:" + "\n" + "\n - Sugar \n - Butter \n - Oil \n - Flour";
-                        advice = "It's okay to enjoy Darry sometimes, but remember to eat him in small amounts. It's important to have a balanced diet with lots of healthy foods. So, choose your food friends wisely!";
+                        character_info = "Beware of Darry the Donut! He may look tempting with his sugary coating, but he holds a secret that's not so sweet. Inside that crispy exterior, Darry is a devious treat, hiding a sinister, chewy piece of dough!";
+                        personality = "Darry may seem friendly, but he's not your healthiest choice. Eating too much of Darry can lead to trouble. His sugary nature can cause bad diseases and give you a dangerous sugar spike. Beware of the risks before becoming friends with Darry!";
+                        advice = "It's important to be careful around Darry. While eating him occasionally is okay, remember to choose healthier options most of the time. Let's make smart choices and prioritize our well-being!";
                         break;
 
                     case "barry":
                         g.drawString("Barry the Broccoli", 50, 80);
                         Image barryScaled = barry.getScaledInstance(350, 350, Image.SCALE_DEFAULT);
                         g.drawImage(barryScaled, 0, 100, this);
+                        character_info = "Say hello to Barry the Broccoli! He's a vibrant green veggie with a big smile. Barry is part of the mighty vegetable family and packed with amazing nutrients that are great for your body!";
+                        personality = "Barry is a true superhero of nutrition. He's rich in vitamins, fiber, and minerals, which can help you grow strong and stay healthy. Eating Barry and other veggies can give you energy, make your bones strong, and even help you see better!";
+                        advice = "Don't be afraid of Barry's unique appearance. Embrace his goodness and include him in your meals. Try steamed or roasted broccoli for a tasty and nutritious treat. Remember, the more colorful veggies you eat, the happier and healthier you'll be!";
                         break;
 
                     case "carla":
                         g.drawString("Carla (the chocolate bar)", 50, 80);
                         Image carlaScaled = carla.getScaledInstance(350, 350, Image.SCALE_DEFAULT);
                         g.drawImage(carlaScaled, 0, 100, this);
+                        character_info = "Watch out for Carla the Chocolate Bar! She may seem yummy with her smooth and creamy texture, but she's not the best choice for your health. Carla is made from sugary chocolate that can cause problems if you eat too much.";
+                        personality = "Carla may be sweet, but she can be a bit tricky. Eating too much chocolate, like Carla, can make you gain weight and hurt your teeth. It can also give you a burst of energy that quickly disappears.";
+                        advice = "Remember, Carla is a treat to enjoy once in a while. It's important to choose healthier snacks most of the time, like fruits or nuts. They give your body what it needs to grow strong and stay healthy.";
                         break;
 
                     case "larry":
                         g.drawString("Larry the Loaf of Bread", 50, 80);
                         Image larryScaled = larry.getScaledInstance(350, 350, Image.SCALE_DEFAULT);
                         g.drawImage(larryScaled, 0, 100, this);
+                        character_info = "Say hello to Larry the Loaf of Bread! He's a wholesome and nutritious friend made from whole grains. Larry is known for his golden crust and soft, fluffy insides.";
+                        personality = "Larry is a health-conscious hero! Being made from whole grains, he's packed with important nutrients like fiber, vitamins, and minerals. Larry helps keep your body strong, gives you energy, and keeps your tummy happy.";
+                        advice = "Embrace Larry as your ally for a balanced diet. Whole grain bread like Larry is a great choice for sandwiches, toast, and more. Make him a regular part of your meals to enjoy his goodness and nourish your body.";
                         break;
                 }
                 
@@ -297,21 +322,21 @@ public class Level1 implements MouseListener {
                 } else if (info_slider == 1) {
                     displayed = personality;
                 } else if (info_slider == 2) {
-                    displayed = ingredients; 
-                } else if (info_slider == 3) {
-                    displayed = advice;
-                }
+                    displayed = advice; 
+                } 
 
                 frame.repaint();
-
+                g.setFont(dogica_text);
+                g.setColor(new Color(255, 190, 48));
+                
                 int lenIn = displayed.length();
                 int start = 0, end = 0, loopCount = 0;
                 /*loop to make sure that the instruction goes on
                 a new line when it reaches the end of the box */
                 while (end < lenIn) {
                     if ( displayed.charAt(end) == ' ') {
-                        if (end - start >= 28) {
-                            g.drawString(displayed.substring(start, end), 360, 130 + 20 * loopCount);
+                        if (end - start >= 25) {
+                            g.drawString(displayed.substring(start, end), 360, 130 + 25 * loopCount);
                             start = end + 1;
                             loopCount++;
                         }
@@ -320,7 +345,7 @@ public class Level1 implements MouseListener {
                 }
                 //when there are leftover words after the line for the last line
                 if (start < lenIn) {
-                    g.drawString(displayed.substring(start), 360, 130 + 20 * loopCount);
+                    g.drawString(displayed.substring(start), 360, 130 + 25 * loopCount);
                 }
 
                 g.setColor(Color.RED);
