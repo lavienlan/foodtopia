cab03456fde7e7efff012cb407bd79986bc6d78f
monellie <monellie6@gmail.com>
2023-06-08 23:49:26 -0400

    Sources started

---
+++ Sources.java
@@ -0,0 +1,84 @@
+/** 
+ * Course Info:</h2>
+ * ICS4U0 with Krasteva, V.
+ *
+ * @version 05/08/2023
+ * @author Monellie Ghaffari-Haghi
+ */
+
+ import java.awt.*;
+ import java.awt.event.*;
+ import javax.swing.*;
+ 
+ public class Sources
+ {
+     JFrame frame = new JFrame("Console");
+     Drawing draw = new Drawing();
+     boolean isTime = false;
+     MainMenu a;
+ 
+     /*
+     * constructor of sources
+     */
+     public Sources ()
+     {
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setSize(800,500);
+         draw.addMouseListener(new ClickHandler());
+         frame.add(draw);
+         frame.setVisible(true);
+     }
+     
+     class ClickHandler extends MouseAdapter
+     {
+         public void mouseClicked (MouseEvent e)
+         {
+             a = new MainMenu();
+             frame.dispose();
+         }
+     }
+ 
+     class Drawing extends JComponent
+     {
+         public void paint (Graphics g)
+         {
+             int x = 0;
+             int y = 30;
+             int x1 = 0;
+             int y1 = 30;
+             int x2 = 40;
+             int y2 = 40;
+             frame.getContentPane().setBackground(new Color(229, 250, 255));
+             Font boldFont = new Font("MonoSpaced", Font.BOLD, 20);
+             g.setFont(boldFont);
+             g.setColor(Color.black);
+             g.drawString("Foodtopia:",240+x1,180+y1);
+             g.setFont(plainFont);
+             g.drawString("Pick Your Food",210+x1,280+y1);
+             Graphics2D g2d;
+             g2d = (Graphics2D) g;
+             g.setColor(new Color(176, 127, 85));
+             g2d.fillRoundRect(20+x, 140+y, 140, 140, 30, 30);
+             g.setColor(new Color(251, 247, 225));
+             g2d.fillRoundRect(30+x, 150+y, 120, 120, 30, 30);
+             g.setColor(new Color(176, 127, 85));
+             g.fillOval(20+x, 109+y, 140, 45);
+             g.setColor(new Color(251, 247, 225));
+             g.fillOval(30+x, 120+y, 120, 35);
+             g.setColor(new Color(255, 217, 102));
+             int[] xs = {580+x2, 720+x2, 650+x2};
+             int[] ys = {110+y2, 110+y2, 260+y2};
+             g.fillPolygon(xs, ys, 3);
+             g.setColor(new Color(230, 188, 152));
+             g2d.fillRoundRect(570+x2, 90+y2, 160, 30, 20, 20);
+             g.setColor(new Color(204, 0, 0));
+             g.fillOval(610+x2, 130+x2, 30,30);
+             g.fillOval(655+x2, 145+x2, 30,30);
+             g.fillOval(635+x2, 180+x2, 30,30);
+         }
+     }
+     public static void main(String[] args) {
+         new Sources();
+     }
+ }
+ 
