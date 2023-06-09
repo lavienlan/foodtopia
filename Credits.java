817e039def51b4f889972727c77b5689bde90c88
lavienlan <130789593+lavienlan@users.noreply.github.com>
2023-06-09 03:03:42 -0400

    created credits page, automatic close

--- Credits.java
+++ Credits.java
@@ -9,25 +9,30 @@ import javax.sound.sampled.Clip;
 public class Credits {
     JFrame frame = new JFrame("Console");
     Drawing draw = new Drawing();
-    Level2 a;
     Font diloWorldL, diloWorldS, pixeltype, dogicaB, dogicaBL;
-    /*
-    * constructor of level 2 introduction
-    */
-    public Credits ()
-    {
+    
+    public Credits() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,500);
+        frame.setSize(800, 500);
         draw.addMouseListener(new ClickHandler());
         try {
-            dogicaB = Font.createFont(Font.TRUETYPE_FONT, new File("dogicapixelbold.ttf")).deriveFont(11f);
-            dogicaBL = Font.createFont(Font.TRUETYPE_FONT, new File("dogicapixelbold.ttf")).deriveFont(40f);
+            dogicaB = Font.createFont(Font.TRUETYPE_FONT, new File("dogicapixelbold.ttf")).deriveFont(30f);
+            dogicaBL = Font.createFont(Font.TRUETYPE_FONT, new File("dogicapixelbold.ttf")).deriveFont(16f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        }
-        catch(IOException | FontFormatException e) {
+        } catch (IOException | FontFormatException e) {
         }
         frame.add(draw);
         frame.setVisible(true);
+
+        // Timer to dispose the frame after 6 seconds
+        int delay = 4000; // 4 seconds
+        Timer timer = new Timer(delay, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        timer.setRepeats(false); // Execute only once
+        timer.start();
     }
 
     class ClickHandler extends MouseAdapter {
@@ -40,47 +45,45 @@ public class Credits {
                 clip.start();
             } catch (Exception ex) {
             }
-            a = new Level2();
-            frame.dispose();
         }
     }
 
-    class Drawing extends JComponent
-    {
-        public void paint (Graphics g)
-        {
-            //background
-            frame.getContentPane().setBackground(new Color(106, 168, 79));
-            //level name
-            g.setFont(dogicaBL);
+    class Drawing extends JComponent {
+        public void paint(Graphics g) {
+            // background
+            frame.getContentPane().setBackground(new Color(255, 204, 228));
+
+            g.setFont(dogicaB);
             g.setColor(Color.BLACK);
-            g.drawString("Level 2",280,180);
-            //ground
+            g.drawString("Thank you for playing!", 110, 130);
+            g.setFont(dogicaBL);
+            g.drawString("Game Presented by Mister Studio", 160, 190);
+            g.drawString("Created by Monellie Ghaffari-Haghi and Rachel Jing", 40, 230);
+            g.drawString("June 2023 ICS4U0 with V. Krasteva", 160, 270);
+
+            // ground
             g.setColor(new Color(182, 215, 168));
             g.fillOval(0, 300, 800, 100);
             g.fillRect(0, 350, 800, 150);
-            //gravel
+            // gravel
             g.setColor(new Color(238, 238, 238));
-            int[] xs = {100, 375, 425, 700};
-            int[] ys = {500, 300, 300, 500};
+            int[] xs = { 100, 375, 425, 700 };
+            int[] ys = { 500, 300, 300, 500 };
             g.fillPolygon(xs, ys, 4);
-            //egg sun
+            // egg sun
             g.setColor(Color.white);
-            g.fillOval(25, 25, 80,80);
-            g.fillOval(20, 50, 20,30);
-            g.fillOval(90, 50, 20,30);
-            g.fillOval(50, 90, 30,20);
-            g.fillOval(50, 20, 30,20);
-            g.fillOval(40, 20, 30,30);
-            g.fillOval(40, 80, 30,30);
+            g.fillOval(25, 25, 80, 80);
+            g.fillOval(20, 50, 20, 30);
+            g.fillOval(90, 50, 20, 30);
+            g.fillOval(50, 90, 30, 20);
+            g.fillOval(50, 20, 30, 20);
+            g.fillOval(40, 20, 30, 30);
+            g.fillOval(40, 80, 30, 30);
             g.setColor(new Color(255, 217, 102));
-            g.fillOval(45, 45, 40,40);
-            //click anywhere
-            g.setFont(dogicaB);
-            g.setColor(new Color(0, 45, 56));
-            g.drawString("Click anywhere to continue.", 280, 445);
+            g.fillOval(45, 45, 40, 40);
         }
     }
+
     public static void main(String[] args) {
         new Credits();
     }
